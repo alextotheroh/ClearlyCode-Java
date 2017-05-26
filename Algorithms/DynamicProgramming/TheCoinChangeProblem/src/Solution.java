fd9056a84e2b01d087b5a165d64383d9af146711
Alex Totheroh <alextotheroh@gmail.com>
2017-05-26 18:01:37 -0400

    committing outline of the coin change problem

---
+++ Algorithms/DynamicProgramming/TheCoinChangeProblem/src/Solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static long getWays(long n, long[] c){
+        // Complete this function
+        return 0l;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        long[] c = new long[m];
+        for(int c_i=0; c_i < m; c_i++){
+            c[c_i] = in.nextLong();
+        }
+        // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+        long ways = getWays(n, c);
+        System.out.println(ways);
+    }
+}
